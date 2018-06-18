ff88c6b2b162a0f20eef1e149d8e894ec0d7e2db
unknown <pedro.failtnt@hotmail.com>
2018-06-18 16:25:20 -0300

    Nova activity criada

---
+++ app/src/main/java/com/example/mptec/primeiroapp/CorTextoActivity.java
@@ -0,0 +1,13 @@
+package com.example.mptec.primeiroapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CorTextoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cor_texto);
+    }
+}
