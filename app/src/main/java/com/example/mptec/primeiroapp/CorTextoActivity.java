8134d0064dcf44b8442a94b270b4d42170cb64ad
unknown <pedro.failtnt@hotmail.com>
2018-06-18 16:37:38 -0300

    Implementações da Cor Texto Activity

--- app/src/main/java/com/example/mptec/primeiroapp/CorTextoActivity.java
+++ app/src/main/java/com/example/mptec/primeiroapp/CorTextoActivity.java
@@ -1,7 +1,13 @@
 package com.example.mptec.primeiroapp;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class CorTextoActivity extends AppCompatActivity {
 
@@ -9,5 +15,25 @@ public class CorTextoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cor_texto);
+
+        final EditText campoedit = (EditText) findViewById(R.id.campoedit);
+
+        Button botaomudacor = (Button) findViewById(R.id.botaomudacor);
+        Button botaoex1 = (Button) findViewById(R.id.botaoex1);
+
+        botaomudacor.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View v){
+                campoedit.setTextColor(Color.parseColor("##FF1493"));
+
+            }
+        });
+
+        botaoex1.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View v){
+                Intent it = new Intent(CorTextoActivity.this,ToastActivity.class);
+                startActivity(it);
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/mptec/primeiroapp/ToastActivity.java
+++ app/src/main/java/com/example/mptec/primeiroapp/ToastActivity.java
@@ -1,5 +1,6 @@
 package com.example.mptec.primeiroapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,11 +18,19 @@ public class ToastActivity extends AppCompatActivity {
         final EditText campoedit = (EditText) findViewById(R.id.campoedit);
 
         Button botaotoast = (Button) findViewById(R.id.botaotoast);
-        Button botao2 = (Button) findViewById(R.id.botao2);
+        Button botaoex2 = (Button) findViewById(R.id.botaoex2);
 
         botaotoast.setOnClickListener(new Button.OnClickListener(){
             public void onClick(View v){
                 Toast.makeText(getApplicationContext(), campoedit.getText(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        botaoex2.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View v){
+                Intent it = new Intent(ToastActivity.this, CorTextoActivity.class);
+                startActivity(it);
 
             }
         });
